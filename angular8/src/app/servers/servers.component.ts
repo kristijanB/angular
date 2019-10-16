69d27193bcd26a9f761e02343ebdf9e6ea268c9b
Kristijan Brechelmacher <kristijan.brechelmacher@ericsson.com>
2019-10-16 15:05:04 +0200

    lekcija 32.

--- angular8/src/app/servers/servers.component.ts
+++ angular8/src/app/servers/servers.component.ts
@@ -2,14 +2,31 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  /*selector: '[app-servers]',*/
+  /*selector: '.app-servers',*/
+  /*template: '<app-server></app-server><app-server></app-server>',*/
+  templateUrl:'servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer=false;
+  serverCreationStatus='No server was created!';
+  serverName='';
 
-  constructor() { }
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer=true;
+    }, 2000)
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer(){
+    this.serverCreationStatus='Server was created!'
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName=(<HTMLInputElement>event.target).value;
+  }
 }
