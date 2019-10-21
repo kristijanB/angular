2c49799dfaaa84db59a501992771ad7e6f4a1d63
Kristijan Brechelmacher <kristijan.brechelmacher@ericsson.com>
2019-10-21 09:05:04 +0200

    lekcija 39

--- angular8/src/app/servers/servers.component.ts
+++ angular8/src/app/servers/servers.component.ts
@@ -12,6 +12,8 @@ export class ServersComponent implements OnInit {
   allowNewServer=false;
   serverCreationStatus='No server was created!';
   serverName='';
+  serverCreated=false;
+  servers= ['TestServer', 'TestServer 2'];
 
   constructor() {
     setTimeout(()=>{
@@ -23,7 +25,9 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
-    this.serverCreationStatus='Server was created!'
+    this.serverCreated=true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus='Server was created! Server name is '+this.serverName;
   }
 
   onUpdateServerName(event: any){
